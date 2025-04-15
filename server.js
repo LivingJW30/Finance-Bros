c6d1b1aaa58ca1429c11e97d718bb56a9f019e49
Debian <dankinahan7@gmail.com>
2025-04-15 19:55:45 +0000

    Signup and Login endpoints with password hashing

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const url = process.env.MONGO_URL;
 const client = new MongoClient(url);
 client.connect();
 
+const db = client.db("finbros"); //Defined db once so that we dont have to constantly re define this
+db.collection("Users").createIndex({ Username: 1 }, { unique: true }); // Makes sure each username is unique
+
+const bcrypt = require('bcrypt'); //For Encryption
+const saltRounds = 10;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) =>
@@ -27,168 +33,64 @@ app.use((req, res, next) =>
     next();
 });
 
-var cardList =
-[
-    'Roy Campanella',
-    'Paul Molitor',
-    'Tony Gwynn',
-    'Dennis Eckersley',
-    'Reggie Jackson',
-    'Gaylord Perry',
-    'Buck Leonard',
-    'Rollie Fingers',
-    'Charlie Gehringer',
-    'Wade Boggs',
-    'Carl Hubbell',
-    'Dave Winfield',
-    'Jackie Robinson',
-    'Ken Griffey, Jr.',
-    'Al Simmons',
-    'Chuck Klein',
-    'Mel Ott',
-    'Mark McGwire',
-    'Nolan Ryan',
-    'Ralph Kiner',
-    'Yogi Berra',
-    'Goose Goslin',
-    'Greg Maddux',
-    'Frankie Frisch',
-    'Ernie Banks',
-    'Ozzie Smith',
-    'Hank Greenberg',
-    'Kirby Puckett',
-    'Bob Feller',
-    'Dizzy Dean',
-    'Joe Jackson',
-    'Sam Crawford',
-    'Barry Bonds',
-    'Duke Snider',
-    'George Sisler',
-    'Ed Walsh',
-    'Tom Seaver',
-    'Willie Stargell',
-    'Bob Gibson',
-    'Brooks Robinson',
-    'Steve Carlton',
-    'Joe Medwick',
-    'Nap Lajoie',
-    'Cal Ripken, Jr.',
-    'Mike Schmidt',
-    'Eddie Murray',
-    'Tris Speaker',
-    'Al Kaline',
-    'Sandy Koufax',
-    'Willie Keeler',
-    'Pete Rose',
-    'Robin Roberts',
-    'Eddie Collins',
-    'Lefty Gomez',
-    'Lefty Grove',
-    'Carl Yastrzemski',
-    'Frank Robinson',
-    'Juan Marichal',
-    'Warren Spahn',
-    'Pie Traynor',
-    'Roberto Clemente',
-    'Harmon Killebrew',
-    'Satchel Paige',
-    'Eddie Plank',
-    'Josh Gibson',
-    'Oscar Charleston',
-    'Mickey Mantle',
-    'Cool Papa Bell',
-    'Johnny Bench',
-    'Mickey Cochrane',
-    'Jimmie Foxx',
-    'Jim Palmer',
-    'Cy Young',
-    'Eddie Mathews',
-    'Honus Wagner',
-    'Paul Waner',
-    'Grover Alexander',
-    'Rod Carew',
-    'Joe DiMaggio',
-    'Joe Morgan',
-    'Stan Musial',
-    'Bill Terry',
-    'Rogers Hornsby',
-    'Lou Brock',
-    'Ted Williams',
-    'Bill Dickey',
-    'Christy Mathewson',
-    'Willie McCovey',
-    'Lou Gehrig',
-    'George Brett',
-    'Hank Aaron',
-    'Harry Heilmann',
-    'Walter Johnson',
-    'Roger Clemens',
-    'Ty Cobb',
-    'Whitey Ford',
-    'Willie Mays',
-    'Rickey Henderson',
-    'Babe Ruth'
-];
+app.post('/api/signup', async (req, res, next) => {
+    //incoming: Username, Password (hashed)
+    //outgoing: error
 
-app.post('/api/addcard', async (req, res, next) =>
-{
-    // incoming: userId, color
-    // outgoing: error
-    const { userId, card } = req.body;
-    const newCard = {Card:card,UserId:userId};
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = { Username: username, Password: hashedPassword, favorites: [] }; //Initializes empty array for favorites
     var error = '';
-    try
-    {
-        const db = client.db();
-        const result = db.collection('cards').insertOne(newCard);
+
+    try {
+        await db.collection('Users').insertOne(newUser);
+        error = 'User Added!';
     }
-    catch(e)
-    {
-        error = e.toString();
+    catch (e) {
+        if (e.code === 11000) {
+            error = 'Username taken. Please try again';
+        }
+        else {
+            error = e.toString();
+        }
     }
-    cardList.push( card );
+
     var ret = { error: error };
     res.status(200).json(ret);
 });
 
-app.post('/api/login', async (req, res, next) =>
-{
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
+app.post('/api/login', async (req, res, next) => {
+    // incoming: username, password
+    // outgoing: username (to display on next screen), error
+
+    const {username, password} = req.body;
     var error = '';
-    const { login, password } = req.body;
-    const db = client.db();
-    const results = await db.collection('users').find({Login:login,Password:password}).toArray();
-    var id = -1;
-    var fn = '';
-    var ln = '';
-    if( results.length > 0 )
-    {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
+    var ret = { username: '', error: '' }; //Defining here so that username isnt null if it doesnt exist
+
+    try {
+        const user = await db.collection("Users").findOne({ Username: username }); //Checks to see if user is in database
+        if(user) {
+            const isMatch = await bcrypt.compare(password, user.Password); //Checks password
+            if(isMatch) {
+                error = 'Login Success';
+                ret.username = user.Username;
+            }
+            else { //Jumps here if password doesnt exist
+                error = 'Invalid Password';
+            }
+        }
+        else { //Jumps here if username doesnt exist
+            error = 'User Not Found.';
+        }
     }
-    var ret = { id:id, firstName:fn, lastName:ln, error:''};
+    catch(e) {
+        error = e.toString();
+    }
+    
+    ret.error = error;
     res.status(200).json(ret);
 });
 
-app.post('/api/searchcards', async (req, res, next) =>
-{
-    // incoming: userId, search// outgoing: results[], error
-    var error = '';
-    const { userId, search } = req.body;
-    var _search = search.trim();
-    const db = client.db();
-    const results = await db.collection('cards').find({"Card":{$regex:_search+'.*'}}).toArray();
-    var _ret = [];
 
-    for( var i=0; i<results.length; i++ )
-    {
-        _ret.push( results[i].Card );
-    }
-
-    var ret = {results:_ret, error:error};
-    res.status(200).json(ret);
-});
 
 app.listen(5001); // start Node + Express server on
